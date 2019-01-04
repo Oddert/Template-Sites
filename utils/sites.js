d76461076666806221b1a4f1de0328c4f2b9660b
Robyn <oddert@gmail.com>
2019-01-04 23:07:48 +0000

    display layout complete save for overrunning text

--- utils/sites.js
+++ utils/sites.js
@@ -1,35 +1,57 @@
 const sites = [
   {
     local: false,
-    title: 'NGEM Network Site',
-    description: 'blah blah',
+    title: 'Cyclemon',
+    description: 'Sample site not designed by oddert co. plz no sue',
     offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
     onsiteURL: null,
-    thumbnail: 'https://lh3.googleusercontent.com/9Yd-wZpkVlvQcIi5FSvxgyA-XwWi6TtXQ6zLvZipsckx-pXj8MrDQQlhC1tTp1Ez3PDTk1R1fWnOJ9LNeOL1kcQmndO_f7pxPbA2zjfPdds6kbJfVBOFQ9DvhWV1OHix9mkEYyIt1ZOBnzToU6-Xp81kFKbM1Z0rVvIO879VMHGtCcyiRoTJNxbw8GbUp_tJXxDRzLcRmF3XvJBc8dul-EBxhY0ERcDbTHsVUoFQyOYSy1AIXnoS8H3G4jPP_Vln5Ro0_eaEJYVRxdyYlrj5BWgf4x2oS4pj-RorZ0hqrlIBM9nL2h0jNdWFSVlV4x00U3fh6u_5nPmIhFD-5QkwGIlGAybtMmN9S1-zNwd9W0F7Td5djUsjm5QHdQgdhYKI2TxbAV1wiLgYWVzTazVwj2ZAoBlPspx7mKADwe1-uayfeQUSIEmzltZdszjF-c_gWNkVheR4VFeHmT3SOkXzH4SjszVihf_nakoR0GQskfEospx7JUoANnzBLRu4bTM95uXVDdF7ny4W_cu6FxWntEqVZkuQReAqDZtIzai-BAS2NqbIrTXQ0kpDov5VaJGJwpnGkCQnuyCALkkbwOOBFqRxaPSjqJ5MCY14CxN6TwVFHul5EkO1-BDmrAbcY1OSgBIP6KDg1BhaQoOx-CTkSVoabQ=w1926-h1014-no'
+    thumbnail: 'https://file.mockplus.com/image/2018/02/9023e426-e0cf-4bcb-be83-ba6697455afe.png',
+    classInserts: ''
   },
   {
     local: false,
     title: 'NGEM Network Site',
-    description: 'blah blah',
+    description: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
     offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
     onsiteURL: null,
-    thumbnail: 'https://lh3.googleusercontent.com/9Yd-wZpkVlvQcIi5FSvxgyA-XwWi6TtXQ6zLvZipsckx-pXj8MrDQQlhC1tTp1Ez3PDTk1R1fWnOJ9LNeOL1kcQmndO_f7pxPbA2zjfPdds6kbJfVBOFQ9DvhWV1OHix9mkEYyIt1ZOBnzToU6-Xp81kFKbM1Z0rVvIO879VMHGtCcyiRoTJNxbw8GbUp_tJXxDRzLcRmF3XvJBc8dul-EBxhY0ERcDbTHsVUoFQyOYSy1AIXnoS8H3G4jPP_Vln5Ro0_eaEJYVRxdyYlrj5BWgf4x2oS4pj-RorZ0hqrlIBM9nL2h0jNdWFSVlV4x00U3fh6u_5nPmIhFD-5QkwGIlGAybtMmN9S1-zNwd9W0F7Td5djUsjm5QHdQgdhYKI2TxbAV1wiLgYWVzTazVwj2ZAoBlPspx7mKADwe1-uayfeQUSIEmzltZdszjF-c_gWNkVheR4VFeHmT3SOkXzH4SjszVihf_nakoR0GQskfEospx7JUoANnzBLRu4bTM95uXVDdF7ny4W_cu6FxWntEqVZkuQReAqDZtIzai-BAS2NqbIrTXQ0kpDov5VaJGJwpnGkCQnuyCALkkbwOOBFqRxaPSjqJ5MCY14CxN6TwVFHul5EkO1-BDmrAbcY1OSgBIP6KDg1BhaQoOx-CTkSVoabQ=w1926-h1014-no'
+    thumbnail: 'https://lh3.googleusercontent.com/9Yd-wZpkVlvQcIi5FSvxgyA-XwWi6TtXQ6zLvZipsckx-pXj8MrDQQlhC1tTp1Ez3PDTk1R1fWnOJ9LNeOL1kcQmndO_f7pxPbA2zjfPdds6kbJfVBOFQ9DvhWV1OHix9mkEYyIt1ZOBnzToU6-Xp81kFKbM1Z0rVvIO879VMHGtCcyiRoTJNxbw8GbUp_tJXxDRzLcRmF3XvJBc8dul-EBxhY0ERcDbTHsVUoFQyOYSy1AIXnoS8H3G4jPP_Vln5Ro0_eaEJYVRxdyYlrj5BWgf4x2oS4pj-RorZ0hqrlIBM9nL2h0jNdWFSVlV4x00U3fh6u_5nPmIhFD-5QkwGIlGAybtMmN9S1-zNwd9W0F7Td5djUsjm5QHdQgdhYKI2TxbAV1wiLgYWVzTazVwj2ZAoBlPspx7mKADwe1-uayfeQUSIEmzltZdszjF-c_gWNkVheR4VFeHmT3SOkXzH4SjszVihf_nakoR0GQskfEospx7JUoANnzBLRu4bTM95uXVDdF7ny4W_cu6FxWntEqVZkuQReAqDZtIzai-BAS2NqbIrTXQ0kpDov5VaJGJwpnGkCQnuyCALkkbwOOBFqRxaPSjqJ5MCY14CxN6TwVFHul5EkO1-BDmrAbcY1OSgBIP6KDg1BhaQoOx-CTkSVoabQ=w1926-h1014-no',
+    classInserts: 'large'
   },
   {
     local: false,
-    title: 'NGEM Network Site',
-    description: 'blah blah',
+    title: 'Cyclemonlongtiltleaaaaaaaaaaa',
+    description: 'Sample site not designed by oddert co. plz no sue',
     offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
     onsiteURL: null,
-    thumbnail: 'https://lh3.googleusercontent.com/9Yd-wZpkVlvQcIi5FSvxgyA-XwWi6TtXQ6zLvZipsckx-pXj8MrDQQlhC1tTp1Ez3PDTk1R1fWnOJ9LNeOL1kcQmndO_f7pxPbA2zjfPdds6kbJfVBOFQ9DvhWV1OHix9mkEYyIt1ZOBnzToU6-Xp81kFKbM1Z0rVvIO879VMHGtCcyiRoTJNxbw8GbUp_tJXxDRzLcRmF3XvJBc8dul-EBxhY0ERcDbTHsVUoFQyOYSy1AIXnoS8H3G4jPP_Vln5Ro0_eaEJYVRxdyYlrj5BWgf4x2oS4pj-RorZ0hqrlIBM9nL2h0jNdWFSVlV4x00U3fh6u_5nPmIhFD-5QkwGIlGAybtMmN9S1-zNwd9W0F7Td5djUsjm5QHdQgdhYKI2TxbAV1wiLgYWVzTazVwj2ZAoBlPspx7mKADwe1-uayfeQUSIEmzltZdszjF-c_gWNkVheR4VFeHmT3SOkXzH4SjszVihf_nakoR0GQskfEospx7JUoANnzBLRu4bTM95uXVDdF7ny4W_cu6FxWntEqVZkuQReAqDZtIzai-BAS2NqbIrTXQ0kpDov5VaJGJwpnGkCQnuyCALkkbwOOBFqRxaPSjqJ5MCY14CxN6TwVFHul5EkO1-BDmrAbcY1OSgBIP6KDg1BhaQoOx-CTkSVoabQ=w1926-h1014-no'
+    thumbnail: 'https://file.mockplus.com/image/2018/02/9023e426-e0cf-4bcb-be83-ba6697455afe.png',
+    classInserts: ''
   },
   {
     local: false,
-    title: 'NGEM Network Site',
-    description: 'blah blah',
+    title: 'Climber',
+    description: 'Sample site not designed by oddert co. plz no sue',
     offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
     onsiteURL: null,
-    thumbnail: 'https://lh3.googleusercontent.com/9Yd-wZpkVlvQcIi5FSvxgyA-XwWi6TtXQ6zLvZipsckx-pXj8MrDQQlhC1tTp1Ez3PDTk1R1fWnOJ9LNeOL1kcQmndO_f7pxPbA2zjfPdds6kbJfVBOFQ9DvhWV1OHix9mkEYyIt1ZOBnzToU6-Xp81kFKbM1Z0rVvIO879VMHGtCcyiRoTJNxbw8GbUp_tJXxDRzLcRmF3XvJBc8dul-EBxhY0ERcDbTHsVUoFQyOYSy1AIXnoS8H3G4jPP_Vln5Ro0_eaEJYVRxdyYlrj5BWgf4x2oS4pj-RorZ0hqrlIBM9nL2h0jNdWFSVlV4x00U3fh6u_5nPmIhFD-5QkwGIlGAybtMmN9S1-zNwd9W0F7Td5djUsjm5QHdQgdhYKI2TxbAV1wiLgYWVzTazVwj2ZAoBlPspx7mKADwe1-uayfeQUSIEmzltZdszjF-c_gWNkVheR4VFeHmT3SOkXzH4SjszVihf_nakoR0GQskfEospx7JUoANnzBLRu4bTM95uXVDdF7ny4W_cu6FxWntEqVZkuQReAqDZtIzai-BAS2NqbIrTXQ0kpDov5VaJGJwpnGkCQnuyCALkkbwOOBFqRxaPSjqJ5MCY14CxN6TwVFHul5EkO1-BDmrAbcY1OSgBIP6KDg1BhaQoOx-CTkSVoabQ=w1926-h1014-no'
+    thumbnail: 'https://file.mockplus.com/image/2018/02/8ad129ae-4629-420a-b57b-cedd8564ee6f.png',
+    classInserts: ''
+  },
+  {
+    local: false,
+    title: 'Climber',
+    description: 'Sample site not designed by oddert co. plz no sue',
+    offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
+    onsiteURL: null,
+    thumbnail: 'https://file.mockplus.com/image/2018/02/8ad129ae-4629-420a-b57b-cedd8564ee6f.png',
+    classInserts: 'horizontal'
+  },
+  {
+    local: false,
+    title: 'Alex Buga',
+    description: 'Sample site not designed by oddert co. plz no sue',
+    offsiteURL: 'https://oddert.github.io/msc/bigger_picture/',
+    onsiteURL: null,
+    thumbnail: 'https://file.mockplus.com/image/2018/02/7525bad1-c663-47ee-8c69-a0d49adef7ae.png',
+    classInserts: 'vertical'
   }
 ]
 
